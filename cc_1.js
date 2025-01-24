18bee9354aea7c0f8580ee93c9b017ee59c42b5e
Derek Ruize <derekruiz@usf.edu>
2025-01-24 14:48:34 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -6,3 +6,12 @@ var isActive = true; // Employees Activity
 console.log(typeof employeeName);
 console.log(typeof employeeID);
 console.log(typeof isActive);
+
+// Task 2
+let productName = "AirPods"; // Product Name
+const productPrice = 250; //Product Price
+var isAvailable = true; //Product Availability
+
+console.log(typeof productName);
+console.log(typeof productPrice);
+console.log(typeof isAvailable);
